8006e9baa1979c71511feee0a0f09eb2c9e38dab
Alex Ungur <alexaandru@gmail.com>
2013-06-06 22:11:34 +0300

    Got the gem to prepare the code for AppConfig, but not actually load it.

--- lib/app_config.rb
+++ lib/app_config.rb
@@ -4,30 +4,35 @@ require 'active_support'
 require 'active_support/core_ext/hash/deep_merge'
 
 unless defined?(AppConfig)
-  AppEnv  = Rails.env unless defined?(AppEnv)
-  AppRoot = Rails.root unless defined?(AppRoot)
-
-  raise "Cannot determine App ENV" if AppEnv.nil?
-  raise "Cannot determine App ROOT" if AppRoot.nil?
-
-  DefaultConfigFile = AppRoot.join('config', 'app_config_default.yml').freeze
-  ConfigFile = AppRoot.join('config', 'app_config.yml').freeze
-  raise "Cannot find neither #{DefaultConfigFile} nor #{ConfigFile}! Aborting." unless
-    File.exist?(DefaultConfigFile) || File.exist?(ConfigFile)
-
   AppConfig = {}
-  def AppConfig.reload_cfg(cfg)
-    File.exist?(cfg) and
-      AppConfig.replace AppConfig.deep_merge(YAML.load(ERB.new(File.read(cfg)).result)[AppEnv])
-  end
-  def AppConfig.reload
-    AppConfig.reload_cfg(DefaultConfigFile)
-    AppConfig.reload_cfg(ConfigFile)
-  end
-  AppConfig.reload
 
-  # Some useful shortcuts into AppConfig
-  AppConfig.keys.each do |key|
-    Kernel.const_set "#{key.split(/[^a-zA-Z]/).map{|x| x.capitalize}.join}Config", AppConfig[key]
+  def AppConfig.reload_cfg(cfg, env, root)
+    File.exist?(cfg) and
+      AppConfig.replace AppConfig.deep_merge(YAML.load(ERB.new(File.read(cfg)).result)[env])
   end
+
+  def AppConfig.reload(env, root)
+    env                 = env.to_s
+    default_config_file = root.join('config', 'app_config_default.yml').freeze
+    config_file         = root.join('config', 'app_config.yml').freeze
+
+    raise "Cannot find neither #{default_config_file} nor #{config_file}! Aborting." unless
+      File.exist?(default_config_file) || File.exist?(config_file)
+
+    AppConfig.reload_cfg(default_config_file, env, root)
+    AppConfig.reload_cfg(config_file, env, root)
+
+    # Some useful shortcuts into AppConfig
+    AppConfig.keys.each do |key|
+      begin
+        const_name = "#{key.split(/[^a-zA-Z]/).map{|x| x.capitalize}.join}Config"
+        Kernel.const_get const_name.intern
+      rescue NameError
+        Kernel.const_set(const_name, AppConfig[key])
+      end
+    end
+  end
+
+# NOTE: Example usage:
+#  AppConfig.reload(Rails.env, Rails.root)
 end
